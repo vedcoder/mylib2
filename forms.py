9c677f95cd891631314dea501d7f3dbd079d96ad
Vidit Gupta <gupta.vidit@icloud.com>
2017-03-04 18:53:07 +0530

    oh my image

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class SignupForm(Form):
+    first_name = StringField('first name',validators=[DataRequired("Please enter first name")])
+    last_name = StringField('last name',validators=[DataRequired("Please enter last name")])
+    email = StringField('Email',validators=[DataRequired("Please enter email"),Email("Please enter your email address.")])
+    password = PasswordField('Password',validators=[DataRequired("Please enter Password"),Length(min=6, message="Passwords must be 6 characters or more.")])
+    submit = SubmitField('sign up')
--- routes.py
+++ routes.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
+from model import db, User
+from forms import SignupForm
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/mylib'
+
+db.init_app(app)
+
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -9,6 +18,28 @@ def index():
 @app.route("/books")
 def books():
   return render_template("books.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+  form = SignupForm()
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+
+      session['email'] = newuser.email
+      return redirect(url_for('home'))
+
+  elif request.method == "GET":
+    return render_template("signup.html",form=form)
+
+@app.route("/home")
+def home():
+  return render_template("home.html")
+
 
 if __name__ == "__main__":
   app.run(debug=True)
